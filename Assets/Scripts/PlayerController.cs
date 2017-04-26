838c331971367a55effc91b6e75c5027673a7271
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-04-25 22:56:00 -0500

    Initial Commit!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 5;
+    public float turnSpeed = 3;
+
+    private Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update() {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        anim.SetFloat("forwardVelocity", y * speed);
+        anim.SetFloat("turnVelocity", x * speed);
+        anim.SetFloat("speed", x * x + y * y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("slashing");
+        }
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anim.SetTrigger("Hit");
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            anim.SetTrigger("dying");
+        }
+    }
+}
