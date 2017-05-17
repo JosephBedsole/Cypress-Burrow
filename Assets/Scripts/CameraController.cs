3c662981f757d64f4af44b9d89997afd2e3c0702
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-05-16 23:19:13 -0500

    Added some more animations for running bach with changed player and camera codes!

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,21 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class CameraController : MonoBehaviour {
+
+    public float moveSpeed = 1;
+    public float turnSpeed = 5;
 
     public Transform target;
     public Vector3 offset;
-    public float speed;
+    public Vector3 lookOffset;
+
 
     void Update()
     {
 
-        if (target == null) return;
+        if (target == null)
+        {
 
-        transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * speed);
+            target = GameObject.FindWithTag("Player").transform;
+
+            return;
+
+        }
 
 
-        transform.LookAt(target);
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            target.position + target.right * offset.x + target.up * offset.y + target.forward * offset.z, 
+            Time.deltaTime * moveSpeed
+            );
+
+        transform.rotation = Quaternion.Slerp(
+            transform.rotation,
+            Quaternion.LookRotation
+            ((target.position + lookOffset) - transform.position),
+            Time.deltaTime * turnSpeed
+            );
+
 
 
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 5;
-    public float turnSpeed = 3;
+    public float maxTurnVelocity = 3f;
+    public float maxForwardVelocity = 5f;
 
     private Animator anim;
     private HealthController health;
+    private Rigidbody body;
 
     void Start() {
         health = GetComponent<HealthController>();
         health.onHealthChanged += AnimateHealth;
+
         anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody>();
     }
+
     void AnimateHealth (float health, float prevHealth, float maxHealth)
     {
         if (health <= 0)
@@ -26,13 +30,34 @@ public class PlayerController : MonoBehaviour {
             anim.SetTrigger("Hit");
         }
     }
-    void Update() {
-        float x = Input.GetAxis("Horizontal");
-        float y = Input.GetAxis("Vertical");
 
-        anim.SetFloat("forwardVelocity", y * speed);
-        anim.SetFloat("turnVelocity", x * speed);
-        anim.SetFloat("speed", x * x + y * y);
+    void Update() {
+        Transform cam = Camera.main.transform;
+        Vector3 targetRight= Input.GetAxis("Horizontal") * cam.right;
+        Vector3 targetForward = Input.GetAxis("Vertical") * cam.forward;
+
+        Vector3 target = targetRight + targetForward;
+        target.y = 0;
+
+        body.velocity = (target).normalized * maxForwardVelocity;
+
+        Vector3 heading = body.velocity.normalized;
+        float forward = Vector3.Dot(heading, transform.forward);
+
+        float speed = body.velocity.magnitude;
+        anim.SetFloat("speed", speed);
+        anim.SetFloat("forwardVelocity", forward * speed);
+        anim.SetFloat("turnVelocity", body.angularVelocity.y);
+
+
+        //float x = Input.GetAxis("Horizontal");
+        //float y = Input.GetAxis("Vertical");
+
+        //anim.SetFloat("forwardVelocity", y * speed);
+        //anim.SetFloat("turnVelocity", x * speed);
+        //anim.SetFloat("speed", x * x + y * y);
+
+        // Actions --------
 
         if (Input.GetButtonDown("Jump"))
         {
@@ -50,5 +75,24 @@ public class PlayerController : MonoBehaviour {
         {
             anim.SetTrigger("grab");
         }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            anim.SetTrigger("pressE");
+        }
     }
+
+    void FixedUpdate()
+    {
+
+        Vector3 v = body.velocity;
+        Vector3 heading = v.normalized;
+        float speed = v.magnitude;
+
+        float turnSpeed = Vector3.Cross(transform.forward, heading).y * maxTurnVelocity;
+        body.angularVelocity = Vector3.up * turnSpeed;
+
+
+
+    }
+
 }
