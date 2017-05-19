3fd4f9dce71bfdc62c71f7eacb79dd6f91a33611
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-05-18 23:21:36 -0500

    Added an alien and gave him an animation tree. We also made an enemy controller.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float maxTurnVelocity = 3.5f;
+    public float maxForwardVelocity = 5f;
+    public float maxSpeedChange = .3f;
+    public float maxTurnSpeedChange = 0.3f;
+    public float wanderAmount = 1;
+    public float preAttackDistance = 4;
+    public float attackDistance = 1;
+
+    private Transform target;
+    private Animator anim;
+    private HealthController health;
+    private Rigidbody body;
+
+    Vector3 targetVelocity;
+    Vector3 targetFacing;
+
+    //Vector3 heading = (target.position - transform.position).normalized;
+    //Vector3 targetVelocity = heading * maxForwardVelocity;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        body = GetComponent<Rigidbody>();
+        target = GameObject.FindWithTag("Player").transform;
+        StartCoroutine("WanderCoroutine");
+	}
+
+    IEnumerator WanderCoroutine ()
+    {
+        bool nearTarget = false;
+
+        while(Vector3.Distance(transform.position, target.position) > 5)
+        {
+            targetVelocity = transform.forward * maxForwardVelocity + Random.insideUnitSphere * wanderAmount;
+            targetFacing = targetVelocity.normalized;
+            targetVelocity = transform.right * maxForwardVelocity;
+            yield return new WaitForSeconds(.5f);
+
+        }
+        StartCoroutine("AttackCoroutine");
+
+    }
+
+    private bool attacking = false;
+
+    IEnumerator AttackCoroutine ()
+    {
+        HealthController playerHealth = target.gameObject.GetComponent<HealthController>();
+        while(playerHealth.health > 0)
+        {
+                Vector3 diff = target.position - transform.position;
+                targetFacing = diff.normalized;
+            if (!attacking)
+            {
+                targetVelocity = transform.right * maxSpeedChange + targetFacing * (preAttackDistance - diff.magnitude);
+                yield return new WaitForEndOfFrame();
+            }
+            else
+            {
+                targetVelocity = transform.forward * maxSpeedChange;
+                anim.SetTrigger("attack");
+            }
+        }
+    }
+
+    void Update()
+    {
+        Vector3 heading = body.velocity.normalized;
+        float forward = Vector3.Dot(heading, transform.forward);
+
+        float speed = body.velocity.magnitude;
+        anim.SetFloat("speed", speed);
+        anim.SetFloat("forwardVelocity", forward * speed);
+        anim.SetFloat("turnVelocity", body.angularVelocity.y);
+
+    }
+
+    void FixedUpdate ()
+    {
+        if (target == null) return;
+
+        Vector3 velocityChange = targetVelocity - body.velocity;
+        velocityChange.y = 0;
+        velocityChange = velocityChange.normalized * Mathf.Clamp(velocityChange.magnitude, 0, maxSpeedChange);
+        body.AddForce(velocityChange, ForceMode.VelocityChange);
+        
+
+        float targetTurnSpeed = Vector3.Cross(transform.forward, targetFacing).y * maxTurnVelocity;
+        float turnSpeedChange = targetTurnSpeed - body.angularVelocity.y;
+        turnSpeedChange = Mathf.Clamp(turnSpeedChange, -maxTurnSpeedChange, maxTurnSpeedChange);
+        body.AddTorque(Vector3.up * turnSpeedChange, ForceMode.VelocityChange);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float maxTurnVelocity = 3f;
+    public float maxSpeedChange = .3f;
     public float maxForwardVelocity = 5f;
-
     private Animator anim;
     private HealthController health;
     private Rigidbody body;
+
+    private Vector3 targetVelocity;
 
     void Start() {
         health = GetComponent<HealthController>();
@@ -36,10 +38,9 @@ public class PlayerController : MonoBehaviour {
         Vector3 targetRight= Input.GetAxis("Horizontal") * cam.right;
         Vector3 targetForward = Input.GetAxis("Vertical") * cam.forward;
 
-        Vector3 target = targetRight + targetForward;
-        target.y = 0;
-
-        body.velocity = (target).normalized * maxForwardVelocity;
+        targetVelocity = targetRight + targetForward;
+        targetVelocity.y = 0;
+        targetVelocity = targetVelocity.normalized * maxForwardVelocity;
 
         Vector3 heading = body.velocity.normalized;
         float forward = Vector3.Dot(heading, transform.forward);
@@ -87,6 +88,11 @@ public class PlayerController : MonoBehaviour {
         Vector3 v = body.velocity;
         Vector3 heading = v.normalized;
         float speed = v.magnitude;
+
+        Vector3 velocityChange = targetVelocity - v;
+        velocityChange = velocityChange.normalized * Mathf.Clamp(velocityChange.magnitude, -maxSpeedChange, maxSpeedChange);
+        velocityChange.y = 0;
+        body.AddForce(targetVelocity, ForceMode.VelocityChange);
 
         float turnSpeed = Vector3.Cross(transform.forward, heading).y * maxTurnVelocity;
         body.angularVelocity = Vector3.up * turnSpeed;
