e83a6597995e25449d21ac4417635c49bd4ff514
Joseph Bedsole <jbedsole@dmi-intertech.org>
2017-05-26 23:39:19 -0500

    Added some trees as an imported unity package. Added a custom made train track set. Added a lookAtMouse script. Deleted some walls. Will be making the boss and adding a second floor soon.

---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour {
+
+    void Update()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+       if (Physics.Raycast(ray, 10))
+        {
+            Vector3 lookHere = ray.GetPoint(10);
+            transform.LookAt(new Vector3(lookHere.x, transform.position.y, lookHere.z));
+        }
+        
+        
+    }
+
+}
